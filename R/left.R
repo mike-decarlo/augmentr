0d922da13168f07403375955fdb28e9b85af3345
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-07-31 07:43:05 -0400

    Typo in left documentation

--- R/left.R
+++ R/left.R
@@ -1,7 +1,7 @@
 #' Extract the \code{n} characters from the left of a string
 #' 
 #' \code{left} uses a specific case of the \code{substr} function to take only
-#'   the right most \code{n} characters from the input string.
+#'   the left most \code{n} characters from the input string.
 #'   
 #' @param x a string
 #' @param n the number of characters from the left of the string to extract
