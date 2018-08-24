39d4100936e0c0e3c1aa92dad44a74106f512144
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-24 14:25:41 -0400

    removed second pack

--- tests/testthat/test-use_packs.R
+++ tests/testthat/test-use_packs.R
@@ -1,7 +1,7 @@
 context("test-use_packs")
 
 test_that("installing packages works", {
-  packs <- c("nycflights13", "ISLR")
+  packs <- c("nycflights13")
   expect_message(
     use_packs(packs)
   )
