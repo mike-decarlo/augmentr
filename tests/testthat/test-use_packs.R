e1baa1959176fb2443d9113e07881c53b8f975cb
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-24 14:19:56 -0400

    no warnings

--- tests/testthat/test-use_packs.R
+++ tests/testthat/test-use_packs.R
@@ -3,8 +3,6 @@ context("test-use_packs")
 test_that("installing packages works", {
   packs <- c("nycflights13", "gapminder")
   expect_message(
-    expect_output(
-      use_packs(packs)
-    )
+    use_packs(packs)
   )
 })
