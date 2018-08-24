43b9cd3a059efd084e145c275942522efc291bc5
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-24 14:25:27 -0400

    added importFrom rstudioapi insertText

--- R/insert_out_addin.R
+++ R/insert_out_addin.R
@@ -1,6 +1,7 @@
 #' Insert \%out\%.
 #' Call this function as an addin to insert \code{ \%out\% } at the cursor
 #'   position.
+#' @importFrom rstudioapi insertText
 #' @export
 insert_out_addin <- function() {
   rstudioapi::insertText(" %out% ")
