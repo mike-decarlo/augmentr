8f0f5b2ef945157f97b0dd6f06903aa52128539a
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-24 14:14:31 -0400

    output and message

--- tests/testthat/test-use_packs.R
+++ tests/testthat/test-use_packs.R
@@ -3,6 +3,8 @@ context("test-use_packs")
 test_that("installing packages works", {
   packs <- c("nycflights13", "gapminder")
   expect_message(
-    use_packs(packs)
+    expect_output(
+      use_packs(packs)
+    )
   )
 })
