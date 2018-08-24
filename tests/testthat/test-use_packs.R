9deb1e85b9a95b7ff7aef17c12652c641d87fb6c
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-24 14:22:33 -0400

    trying a different package

--- tests/testthat/test-use_packs.R
+++ tests/testthat/test-use_packs.R
@@ -1,7 +1,7 @@
 context("test-use_packs")
 
 test_that("installing packages works", {
-  packs <- c("nycflights13", "gapminder")
+  packs <- c("nycflights13", "ISLR")
   expect_message(
     use_packs(packs)
   )
