01e864a050897c0e9e25bba5a1cfd9b096731cef
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-12-03 09:01:55 -0500

    updates to inst_packs

--- R/inst_packs.R
+++ R/inst_packs.R
@@ -1,8 +1,8 @@
 #' Multiple package installation (without loading)
 #' The \code{inst_packs} function takes a single command, a list of package
 #'   names, and will install them, if not already installed.
-#' @param packlist character vector of the names of packages whose current
-#'   versions should be downloaded from the repositories.
+#' @param ... one or more character strings of package names
+#'   to be downloaded from the repositories.
 #' @param dependencies logical indicating whether to also install uninstalled
 #'   packages which these packages depend on/link to/import/suggest.
 #' @keywords pack install package
@@ -10,12 +10,11 @@
 #' @export
 inst_packs <- function(..., dependencies = TRUE) {
   packages <- c(...)
-  if (is.null(packlist)) {
+  if (is.null(packages)) {
     stop(
-      "\nArgument 'packlist' must be of length >= 1.\n"
+      "\nMust include at least one package to install.\n"
     )
   }
-  packages <- c(packlist)
   for (i in 1:length(packages)) {
     if (!is.element(packages[i], installed.packages()[, 1])) {
       message(paste("Package", packages[i], "not found, installing..."))
@@ -25,6 +24,8 @@ inst_packs <- function(..., dependencies = TRUE) {
         , verbose = TRUE
         , repos = "http://cran.us.r-project.org"
         )
+    } else {
+      message(paste("Package", packages[i], "found and will not be installed."))
     }
   }
 }
--- tests/testthat/test-inst_packs.R
+++ tests/testthat/test-inst_packs.R
@@ -1,17 +1,12 @@
 context("test-inst_packs")
 
 test_that("installing packages works", {
-  packs <- c("nycflights13")
   expect_message(
-    inst_packs(packs)
+    inst_packs("nycflights13")
   )
 })
 
 test_that("installing with packs = NULL errors", {
-  packs <- NULL
-  expect_error(
-    inst_packs(packs)
-  )
   expect_error(
     inst_packs()
   )
