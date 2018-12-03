e15cbdc3f47730682e8562d855a993c9a27a7657
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-12-03 08:45:53 -0500

    updates to use_packs

--- R/use_packs.R
+++ R/use_packs.R
@@ -1,20 +1,30 @@
 #' Package (multiple) installing and loading function
 #' \code{use_packs} takes a single command, a list of package names, and will
 #'   install them, if not already installed, and then load them.
-#' @param packlist character vector of the names of packages whose current
+#' @param ... one or more character strings of package names
 #'   versions should be downloaded from the repositories.
 #' @param dependencies logical indicating whether to also install uninstalled
 #'   packages which these packages depend on/link to/import/suggest.
+#' @param force logical indicating whether packages should be forced to be
+#'   installed/updated
 #' @keywords use pack install package
 #' @importFrom utils install.packages installed.packages
 #' @export
-use_packs <- function(packlist = c(), dependencies = TRUE) {
-  packages <- c(packlist)
+use_packs <- function(..., dependencies = TRUE, force = FALSE) {
+  packages <- c(...)
   if (is.null(packages)) {
     stop("Error: 'packlist' must be length >= 1.\n")
   } else {
-    for (i in 1:length(packages)) {
-      if (!is.element(packages[i], installed.packages()[, 1])) {
+    for (i in seq_along(packages)) {
+      if (force  == TRUE) {
+        message(paste0("\nInstalling ", packages[i], " specified packages.\n"))
+        install.packages(
+          packages[i]
+          , dependencies = dependencies
+          , repos = "http://cran.us.r-project.org"
+        )
+      }
+      else if (!is.element(packages[i], installed.packages()[, 1])) {
         message(paste("Package", packages[i], "not found, installing..."))
         install.packages(
           packages[i]
