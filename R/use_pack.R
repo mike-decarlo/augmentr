11b6ee12dc1eec16cfd1e3d014f9cf86aa856612
Mike <8523449+wolfm4ne@users.noreply.github.com>
2019-02-22 12:46:14 -0500

    "force" option on use_pack

--- R/use_pack.R
+++ R/use_pack.R
@@ -1,31 +1,35 @@
 #' Package installing and loading function
 #' \code{use_pack} takes a single command, the name of a package, and will
 #'   install it, if not already installed, and then load it.
-#' @param pack a string; the name of a package to be installed
-# @param update a boolean; if \code{TRUE} then any available updates for the
-#   package will be installed, if \code{FALSE} then updates will not be
-#   installed
+#' @param package a string; the name of a package to be installed
 #' @param dependencies a boolean; if \code{TRUE} dependency packages will also
 #'   be installed, if \code{FALSE} dependency packages will not be installed
+#' @param force logical indicating whether packages should be forced to be
+#'   installed/updated
 #' @keywords use pack install package
 #' @importFrom utils install.packages installed.packages new.packages
 #' @export
-use_pack <- function(pack = NULL, dependencies = TRUE) {
-  pack <- c(pack)
-  if (is.null(pack)) {
+use_pack <- function(package = NULL, dependencies = TRUE, force = FALSE) {
+  package <- c(package)
+  if (is.null(package)) {
     stop(
       "\nMust include a package to install.\n"
     )
-  } else {
-      if (!is.element(pack, installed.packages()[, 1])) {
-      message(paste("Package", pack, "not found, installing..."))
-      install.packages(
-        pack
-        , dependencies = dependencies
-        , repos = "http://cran.us.r-project.org"
-        )
-      }
-    message(paste0("Loading package ", pack, "..."))
-    library(pack, character.only = TRUE)
+  } else if (force == TRUE) {
+    message(paste0("\nInstalling ", package, " specified package.\n"))
+    install.packages(
+      package
+      , dependencies = dependencies
+      , repos = "http://cran.us.r-project.org"
+    )
+  } else if (!is.element(package, installed.packages()[, 1])) {
+    message(paste("Package", package, "not found, installing..."))
+    install.packages(
+      package
+      , dependencies = dependencies
+      , repos = "http://cran.us.r-project.org"
+      )
   }
+  message(paste0("Loading package ", package, "..."))
+  library(package, character.only = TRUE)
 }
